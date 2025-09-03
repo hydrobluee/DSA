c5ebf407d228ce881273f77306994edd54fba9a5
Vivek Verma <139622791+hydrobluee@users.noreply.github.com>
2025-09-03 07:25:22 +0530

    Time: 30 ms (13.16%) | Memory: 45.1 MB (9.21%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int solve(boolean[][] grid, int r, int c) {
+        boolean[][] invalid = new boolean[51][51];
+        int cnt = 0;
+        int n=50;
+        for(int i=r; i<=50; i++) {
+            for(int j=c; j<=n; j++) {
+                if(i==r && j==c) continue;
+                if(grid[i][j] && !invalid[i][j]) {
+                    cnt++;
+                    n = j-1;
+                } 
+            }
+        }
+        return cnt;
+    }
+    public int numberOfPairs(int[][] points) {
+        boolean[][] grid = new boolean[51][51];
+        for(int[] point : points) {
+            grid[50-point[1]][point[0]] = true;
+        }
+        int cnt = 0;
+        for(int i=0; i<=50; i++) {
+            for(int j=0; j<=50; j++) {
+                if(grid[i][j]) {
+                    cnt += solve(grid, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+}
